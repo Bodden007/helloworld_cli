e526186d5618cb12570278ed7b90bc58eb74d5a2
Bodden007 <92564748+Bodden007@users.noreply.github.com>
2023-05-08 21:58:23 +0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CPP_DEV/Learning/helloworld_cli
-# Build directory: C:/CPP_DEV/Learning/helloworld_cli/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_cpp]=] "helloworld_cli")
-set_tests_properties([=[test_cpp]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/CPP_DEV/Learning/helloworld_cli/CMakeLists.txt;26;add_test;C:/CPP_DEV/Learning/helloworld_cli/CMakeLists.txt;0;")
